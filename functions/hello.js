515923c1a9048f561ecae0bd98835b17d0c77a22
Francois <f.parrou@agence42.fr>
2022-08-04 16:43:36 +0200

    api test

--- functions/hello.js
+++ functions/hello.js
@@ -18,7 +18,7 @@ exports.handler = async function (event, context) {
       .then(() => {
         return {
             statusCode: 200,
-            body: JSON.stringify('Email sent')
+            body: JSON.stringify('Email sent successfully')
         };
       })
       .catch((error) => {
