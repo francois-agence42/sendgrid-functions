8241e003459560d4cf20e16ff23c2ed63207992f
Francois <f.parrou@agence42.fr>
2022-08-04 15:57:29 +0200

    test mail

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,28 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
 exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Coucou mohamed" }),
-    };
+    const msg = {
+        to: 'f.parrou@agence42.fr',
+        from: 'test@example.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: `<strong>Good</strong>`,
+      }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Coucou mohamed" }),
+        };
+      })
+      .catch((error) => {
+        console.error(error)
+        return {
+            statusCode: error.response.statusCode,
+            body: JSON.stringify({ message: error.message }),
+          };
+      })
   };
