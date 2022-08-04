73913a8afbb0f6f3ab9ed43f23554e65c5a8279f
Francois <f.parrou@agence42.fr>
2022-08-04 16:48:38 +0200

    another try

--- functions/hello.js
+++ functions/hello.js
@@ -1,31 +1,65 @@
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const client = require('@sendgrid/mail');
+const {
+  SENDGRID_API_KEY,
+  SENDGRID_TO_EMAIL,
+  SENDGRID_FROM_EMAIL,
+} = process.env;
 
-exports.handler = async function (event, context) {
-    console.log(sgMail)
-    const msg = {
-        to: 'f.parrou@agence42.fr',
-        from: 'test@example.com',
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: `<strong>Good</strong>`,
-      }
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log('Email sent')
-      })
-      .then(() => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify('Email sent successfully')
-        };
-      })
-      .catch((error) => {
-        console.error(error)
-        // return {
-        //     statusCode: error.response.statusCode,
-        //     body: JSON.stringify({ message: error.message }),
-        //   };
-      })
+exports.handler = async function (event, context, callback) {
+//   const { message, senderEmail, senderName } = JSON.parse(event.body);
+  client.setApiKey(SENDGRID_API_KEY);
+
+  const data = {
+    to: 'f.parrou@agence42.fr',
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: `<strong>Good</strong>`,
   };
+
+  try {
+    await client.send(data);
+    return {
+      statusCode: 200,
+      body: 'Message sent',
+    };
+  } catch (err) {
+    return {
+      statusCode: err.code,
+      body: JSON.stringify({ msg: err.message }),
+    };
+  }
+};
+
+
+// const sgMail = require('@sendgrid/mail')
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+// exports.handler = async function (event, context) {
+//     console.log(sgMail)
+//     const msg = {
+//         to: 'f.parrou@agence42.fr',
+//         from: 'test@example.com',
+//         subject: 'Sending with SendGrid is Fun',
+//         text: 'and easy to do anywhere, even with Node.js',
+//         html: `<strong>Good</strong>`,
+//       }
+//     sgMail
+//       .send(msg)
+//       .then(() => {
+//         console.log('Email sent')
+//       })
+//       .then(() => {
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify('Email sent successfully')
+//         };
+//       })
+//       .catch((error) => {
+//         console.error(error)
+//         // return {
+//         //     statusCode: error.response.statusCode,
+//         //     body: JSON.stringify({ message: error.message }),
+//         //   };
+//       })
+//   };
