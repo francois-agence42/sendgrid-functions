5eade0ee86e03fb264d583b23c6782a0e1a1f80d
Francois <f.parrou@agence42.fr>
2022-08-04 15:02:48 +0200

    first commit

---
+++ components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Sendgrid + netlify functions</NuxtLink>
+        </li>
+      </ul>
+      <!-- <ul>
+        <li><a href="#">Lien</a></li>
+      </ul> -->
+    </nav>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="container">
+    <Nav />
+    <Form />
+  </main>
+</template>
+
+<script>
+import Nav from "../components/Nav.vue";
+export default {
+  name: "IndexPage",
+  components: { Nav },
+};
+</script>
