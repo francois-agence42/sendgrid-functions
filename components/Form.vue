a495ab3cf97511fd6e946f4be5e53a02696acba3
Francois <f.parrou@agence42.fr>
2022-08-05 11:20:23 +0200

    test with form mail content

--- components/Form.vue
+++ components/Form.vue
@@ -1,67 +1,88 @@
 <template>
-  <form>
-    <!-- Grid -->
-    <div class="grid">
-      <!-- Markup example 1: input is inside label -->
-      <label for="firstname">
-        First name
-        <input
-          type="text"
-          id="firstname"
-          name="firstname"
-          placeholder="First name"
-          required
-        />
-      </label>
+  <section>
+    <form @submit.prevent="sendMail()">
+      <div class="grid">
+        <label for="firstname">
+          First name
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            placeholder="First name"
+            v-model="firstname"
+            required
+          />
+        </label>
 
-      <label for="lastname">
-        Last name
-        <input
-          type="text"
-          id="lastname"
-          name="lastname"
-          placeholder="Last name"
-          required
-        />
-      </label>
-    </div>
+        <label for="lastname">
+          Last name
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            placeholder="Last name"
+            v-model="lastname"
+            required
+          />
+        </label>
+      </div>
 
-    <!-- Markup example 2: input is after label -->
-    <label for="email">Email address</label>
-    <input
-      type="email"
-      id="email"
-      name="email"
-      placeholder="Email address"
-      required
-    />
-    <small>We'll never share your email with anyone else.</small>
+      <label for="email">Email address</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Email address"
+        v-model="email"
+        required
+      />
+      <label for="message">your message</label>
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Your message"
+        v-model="message"
+        rows="5"
+        required
+      />
+      <!-- <small>Hey</small> -->
 
-    <!-- Button -->
-    <button type="submit" @submit.prevent="sendMail()">Submit</button>
-  </form>
+      <!-- Button -->
+      <button type="submit">Submit</button>
+    </form>
+  </section>
 </template>
 
 <script>
 export default {
+  data: function () {
+    return {
+      formName: "Contact",
+      message: "",
+      firstname: "",
+      lastname: "",
+      email: "",
+    };
+  },
   methods: {
     sendMail() {
-      // fetch('https://sendgridfunctions.netlify.app/.netlify/functions/hello', {
-      //   method: 'POST',
-      //   body: JSON.stringify({
-      //     firstname: this.$refs.firstname.value,
-      //     lastname: this.$refs.lastname.value,
-      //     email: this.$refs.email.value,
-      //   }),
-      // })
-      //   .then(res => res.json())
-      //   .then(data => {
-      //     console.log(data);
-      //   })
-      //   .catch(err => {
-      //     console.log(err);
-      //   });
-      console.log("sendMail");
+      fetch("https://sendgridfunctions.netlify.app/.netlify/functions/hello", {
+        method: "POST",
+        body: JSON.stringify({
+          formName: this.formName,
+          firstname: this.firstname,
+          lastname: this.lastname,
+          email: this.email,
+          message: this.message,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 };
--- components/Nav.vue
+++ components/Nav.vue
@@ -1,14 +1,12 @@
 <template>
-  <section>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Sendgrid + netlify functions</NuxtLink>
-        </li>
-      </ul>
-      <!-- <ul>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Sendgrid + netlify functions</NuxtLink>
+      </li>
+    </ul>
+    <!-- <ul>
         <li><a href="#">Lien</a></li>
       </ul> -->
-    </nav>
-  </section>
+  </nav>
 </template>
